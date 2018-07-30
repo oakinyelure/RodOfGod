6e4f2ced7c8d2ce1684287d1d77d0167bb60aed8
Olusegun Akinyelure <oakinyelure@hotmail.com>
2018-07-30 17:09:00 -0400

    Revert "Created home page component (#2)"
    This reverts commit c59d0d83297bd279a5528fc1627e6e331359a54d.

--- RodOfGod/frontend/src/app/app.module.ts
+++ RodOfGod/frontend/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
