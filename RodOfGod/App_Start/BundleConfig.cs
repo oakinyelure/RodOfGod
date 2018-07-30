6b7ec014de5cf5399a99194260d9cd8c8ec84a23
oakinyelure <oakinyelure@hotmail.com>
2018-07-30 04:43:29 -0400

    Merged build from oakinyelure

--- RodOfGod/App_Start/BundleConfig.cs
+++ RodOfGod/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace RodOfGod
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /**
+             * bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -25,6 +26,17 @@ namespace RodOfGod
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+    **/
+
+            bundles.Add(new Bundle("~/AngularBundles/ScriptBundle").Include(
+                "~/AngularBundles/inline.*",
+                "~/AngularBundles/polyfills.*",
+                "~/AngularBundles/scripts.*",
+                "~/AngularBundles/vendor.*",
+                "~/AngularBundles/main.*"
+                ));
+
+            bundles.Add(new Bundle("~/AngularBundles/StyleBundle").Include("~/AngularBundles/styles.*"));
         }
     }
 }
