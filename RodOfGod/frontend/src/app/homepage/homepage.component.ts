6a31d802a4ad2dfd9845ae5619835fa2b09636ca
oakinyelure <oakinyelure@hotmail.com>
2018-07-30 22:00:09 -0400

    Readded angular cli

--- RodOfGod/frontend/src/app/homepage/homepage.component.ts
+++ RodOfGod/frontend/src/app/homepage/homepage.component.ts
@@ -10,8 +10,8 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   isFindDistanceClicked: boolean = false;
-
   userDistanceToChurch:number;
+  errorMessage: string;
 
   ngOnInit() {
   }
