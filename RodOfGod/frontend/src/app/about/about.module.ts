b1b6ee19286a4d2b03a26788730a784b9437a5b0
kanako001 <kunleanako78@gmail.com>
2018-08-25 00:08:54 -0400

    Created page tab and script

--- RodOfGod/frontend/src/app/about/about.module.ts
+++ RodOfGod/frontend/src/app/about/about.module.ts
@@ -4,14 +4,19 @@ import { AboutComponent } from './about.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { AboutLeadershipComponent } from './about-leadership/about-leadership.component';
+import { AboutVisionComponent } from './about-vision/about-vision.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, AboutLeadershipComponent, AboutVisionComponent],
   exports: [AboutComponent],
   bootstrap: [AboutComponent]
 })
