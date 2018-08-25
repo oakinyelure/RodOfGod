7257543617b2f6ae3c93aa1169ec950374516517
kanako001 <kunleanako78@gmail.com>
2018-08-24 22:53:54 -0400

    misc

--- RodOfGod/frontend/src/app/about/about.module.ts
+++ RodOfGod/frontend/src/app/about/about.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    FormsModule
   ],
   declarations: [AboutComponent],
-  exports: [AboutComponent]
+  exports: [AboutComponent],
+  bootstrap: [AboutComponent]
 })
 export class AboutModule { }
