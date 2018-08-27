cc105ede37be3fb758a2c1172b2cd0d380f00aa9
Olusegun Akinyelure <oakinyelure@hotmail.com>
2018-08-27 11:24:05 -0400

    Fix for about page
    Modified about page. Included content. Page needs significant refactoring. Developer copied code from the internet without understanding the intent (#15)

--- RodOfGod/frontend/src/app/about/about.component.ts
+++ RodOfGod/frontend/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   displayVision: boolean = true;
+
+  @Input() page: string = "about";
 
   currentPage: string = "vision";
 
