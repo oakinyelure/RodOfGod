9db3af188e645cf65dee6a8344a20aaed0b382e8
kanako001 <kunleanako78@gmail.com>
2018-09-02 19:38:36 -0400

    modified the vision and leadership pages

--- RodOfGod/frontend/src/app/shared/shared.module.ts
+++ RodOfGod/frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TopNavComponent],
+  declarations: [TopNavComponent, FooterComponent],
   exports: [TopNavComponent]
 })
 export class SharedModule { }
