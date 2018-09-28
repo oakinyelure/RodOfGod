6b3cf5638717cf97ad217b11c5f37ff65c784f5f
oakinyelure <oakinyelure@hotmail.com>
2018-09-28 01:16:41 -0400

    auth startup configuration

---
+++ RodOfGod/App_Start/OwinStartup.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+using RodOfGod;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(OwinStartup))]
+namespace RodOfGod
+{
+
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ RodOfGod/Models/User/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RodOfGod.Models.User
+{
+    public class User
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at leat {2} chareacters long. ", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password",ErrorMessage = "The password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
